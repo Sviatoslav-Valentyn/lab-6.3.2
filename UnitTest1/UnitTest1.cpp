c2712087f1c7914bdde828bf0a9bbe5adef0ca1f
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2021-11-04 19:14:00 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\lab 6.3.2\Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int A[] = { 1, 7, 6, 9 };
+			Sum(A, 4, 0);
+			Assert::AreEqual(A[2],6);
+		}
+	};
+}
---
+++ lab 6.3.2/Source.cpp
@@ -0,0 +1,76 @@
+// Варіант 6(Рекурсивний спосіб)
+#include <iostream> 
+#include <iomanip> 
+#include <time.h> 
+
+using namespace std;
+
+void Init(int* b, const int n, const int A, const int B, int i)
+{
+	if (i < n)
+	{
+		b[i] = A + rand() % ( B - A + 1);
+		return Init(b, n, A, B, i + 1);
+	}
+}
+
+template<typename T>
+void Init(T* b, const int n, const int A, const int B, int i)
+{
+	if (i < n)
+	{
+		b[i] = A + rand() % (B - A + 1);
+		return Init(b, n, A, B, i + 1);
+	}
+}
+
+void Print(int* b, const int n, int i)
+{
+    cout << "{ ";
+    for (int i = 0; i < n; i++)
+        cout << b[i] << " ,";
+    cout << " }";
+}
+
+template<typename T>
+void Print(T* b, const int n, int i)
+{
+    cout << "{ ";
+    for (int i = 0; i < n; i++)
+        cout << b[i] << " ,";
+    cout << " }";
+}
+
+int Sum(int b[], const int n,int i)
+{
+	if (i < n)
+	     return b[i] + Sum(b, n, i + 1);
+	else
+	     return 0;
+}
+
+template<typename T>
+int Sum(T b[], const int n,int i)
+{
+	if (i < n)
+	     return b[i] + Sum(b, n, i + 1);
+	else
+	     return 0;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int n;
+	cout << "n = "; cin >> n;
+	int* b = new int[n];
+	int A = -10;
+	int B = 20;
+	Init(b, n, A, B, 0);
+	Print(b, n, 0);
+	Sum(b, n, 0);
+	cout << "S = " << Sum(b, n, 0) << endl;
+	delete[] b;
+	b = nullptr;
+	return 0;
+}
